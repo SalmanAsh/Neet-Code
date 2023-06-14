203065fd6bfd47ad8485202b169f65800245dd1b
SalmanAsh <102732552+SalmanAsh@users.noreply.github.com>
2023-06-14 19:02:36 +0100

    java solution

--- Binary Search/MinRotatedSearch.java
+++ Binary Search/MinRotatedSearch.java
@@ -1,4 +1,4 @@
-public class rotatedSearch{
+public class MinRotatedSearch{
     public static void main(String[] args) {
         int[] nums = {4, 5, 6, 7, 0, 1, 2};
         System.out.println(search(nums));
