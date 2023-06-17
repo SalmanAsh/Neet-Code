ccbf8822e234be8e51493dc617bbbba91eb770dc
SalmanAsh <102732552+SalmanAsh@users.noreply.github.com>
2023-06-17 11:00:23 +0100

    merge array

--- Linked List/merge_two_sorted_list.py
+++ Linked List/merge_two_sorted_list.py
@@ -1,2 +1,20 @@
-# class Solution:
-#     def merge(self,  list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+class Solution:
+    def merge(self,  list1: ListNode, list2: ListNode) -> Optional[ListNode]:
+        dummy = ListNode()
+        tail = dummy
+        
+        while(list1 and list2):
+            if (list1.val < list2.val):
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+
+        if (not list1):
+            tail.next = list2
+        elif(not list2):
+            tail.next = list1
+                
+        return dummy.next
