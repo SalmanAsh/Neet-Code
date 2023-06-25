333791ba2e8ca8bc516d21d6734cd42229aae1d5
SalmanAsh <102732552+SalmanAsh@users.noreply.github.com>
2023-06-25 11:40:51 +0100

    java solution started

---
+++ Trees/ConstructBT.java
@@ -0,0 +1,7 @@
+public class ConstructBT {
+    public static void main(String[] args){}
+    public static TreeNode buildTree(int[] preorder, int[] inorder){
+        // TreeNode root = pre;
+        return null;
+    }
+}
