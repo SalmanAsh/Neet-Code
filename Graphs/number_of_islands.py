7c2bdddffa2b6c068653b1447ae3417d2e02a52d
SalmanAsh <102732552+SalmanAsh@users.noreply.github.com>
2023-07-01 09:57:32 +0100

    Breath first search for visited nodes

--- Graphs/number_of_islands.py
+++ Graphs/number_of_islands.py
@@ -17,18 +17,17 @@ class Solution:
                 row, col = q.popleft() #use q.pop() for DFS
                 directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
                 
-                for dr, dc in direction:
+                for dr, dc in directions:
                     r = row + dr
                     c = col + dc
                     if (r) in range(rows) and (c) in range(cols) and grid[r][c] == "1" and (r, c) not in visited:
                         q.append((r, c))
                         visited.add((r, c))
                         
-                        
-                
-        
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == "1" and (r, c) not in visited:
                     bfs(r, c)
                     islands += 1
+        
+        return islands
