2d756198f1002f8b5422749a358a8b96c8321998
SalmanAsh <102732552+SalmanAsh@users.noreply.github.com>
2023-07-04 11:54:34 +0100

    house robbers algorithms

--- 1-D Dynamic Programming/house_robber.py
+++ 1-D Dynamic Programming/house_robber.py
@@ -2,6 +2,7 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         rob1, rob2 = 0, 0
         
+        #[rob1, rob2, n, n + 1, ...]
         for n in nums:
             temp = max(n + rob1, rob2)
             rob1 = rob2
