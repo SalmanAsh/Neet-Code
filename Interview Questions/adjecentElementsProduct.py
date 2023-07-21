4b4cd9ddee13240b3d511831be56aaaf2f38d305
SalmanAsh <102732552+SalmanAsh@users.noreply.github.com>
2023-07-21 12:39:54 +0100

    Solved

---
+++ Interview Questions/adjecentElementsProduct.py
@@ -0,0 +1,5 @@
+def solution(inputArray):
+    res = inputArray[0] * inputArray[1]
+    for l in range(len(inputArray)):
+        res = max(res, inputArray[l] * inputArray[l + 1])
+    return res
--- Interview Questions/checkPalindrome.py
+++ Interview Questions/checkPalindrome.py
@@ -1,2 +1,31 @@
 def solution(inputString):
-    pass
+    # Even Length words
+    l, r = (len(inputString) // 2) - 1, len(inputString) // 2
+    while(l >= 0 and r < len(inputString)):
+        if (inputString[l] != inputString[r]):
+            break
+        elif(l == 0 and r == len(inputString) - 1):
+            return True
+        
+        l -= 1
+        r += 1
+    
+    # Odd Length words
+    l = len(inputString) // 2
+    r = l
+    while(l >= 0 and r < len(inputString)):
+        if (inputString[l] != inputString[r]):
+            return False
+        l -= 1
+        r += 1
+    
+    return True
+
+def is_palindrome(s: str) -> bool:
+    length = len(s)
+    
+    for i in range(length//2):
+        if (s[i] != s[length - i - 1]):
+            return False
+    
+    return True
