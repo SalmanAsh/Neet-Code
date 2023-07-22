d9a2a08da36ec295b4a90faeb6ad705527ab9cd0
SalmanAsh <102732552+SalmanAsh@users.noreply.github.com>
2023-07-22 10:12:53 +0100

    solution

--- Interview Questions/adjecentElementsProduct.py
+++ Interview Questions/adjecentElementsProduct.py
@@ -1,5 +1,7 @@
 def solution(inputArray):
     res = inputArray[0] * inputArray[1]
+    
     for l in range(len(inputArray)):
         res = max(res, inputArray[l] * inputArray[l + 1])
+        
     return res
