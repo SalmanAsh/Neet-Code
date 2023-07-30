222faa4119e965146c0c1ffd595d79a0a04b1594
SalmanAsh <102732552+SalmanAsh@users.noreply.github.com>
2023-07-30 11:17:26 +0100

    Remove Duplicates

---
+++ LC26.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+
+public class LC26 {
+    public int removeDuplicates(int[] nums){
+        if(nums.length == 0) return 0;
+
+        int l = 1;
+        for(int r = 1; r < nums.length; r++){
+            if (nums[r] != nums[r - 1]){
+                nums[l] = nums[r];
+                l++;
+            }
+        }
+        return l;
+    }
+}
