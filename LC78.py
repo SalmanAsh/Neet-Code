82b950986478ae48a3ec75b1fcf8937704147d7a
SalmanAsh <102732552+SalmanAsh@users.noreply.github.com>
2023-08-14 14:36:37 +0100

    Subsets backtracking approach

--- LC78.py
+++ LC78.py
@@ -1,6 +1,23 @@
 class Solution:
-    def subsets(self, nums: List[int]) -> List[int]:
-        pass
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        subset = []
+        
+        def dfs(i):
+            if  i >= len(nums):
+                res.append(subset.copy())
+                return
+            
+            # Include
+            subset.append(nums[i])
+            dfs(i + 1)
+            
+            # Not Include
+            subset.pop()
+            dfs(i + 1)
+        
+        dfs(0)
+        return res
 
 
 
