ed21d3c9f9763e2ab43dffd2aaa64a3747f13329
h45123sa <salman.ashraf@student.manchester.ac.uk>
2024-08-08 20:39:05 +0100

    LC 739

--- Python/DailyTemperatures.py
+++ Python/DailyTemperatures.py
@@ -12,4 +12,14 @@ class DailyTemperatures:
         return res
 
     def solution2(self, temperatures: List[int]) -> List[int]:
-        pass
+        res = [0] * len(temperatures)
+        stack = [] # pair: [temp, index]
+
+        for i, t in enumerate(temperatures):
+            while stack and stack[-1][0] < t:
+                stackT, stackI = stack.pop()
+                res[stackI] = i - stackI
+            
+            stack.append([t, i])
+        
+        return res
