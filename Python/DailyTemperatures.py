a24a401246f5db6850f0ab0bb850224808fd4f88
h45123sa <salman.ashraf@student.manchester.ac.uk>
2024-08-10 12:29:10 +0100

    LC 739

--- Python/DailyTemperatures.py
+++ Python/DailyTemperatures.py
@@ -1,25 +1,33 @@
+# pylint: disable:missing-module-docstring,invalid-name
 class DailyTemperatures:
-    def solution1(self, temperatures: List[int]) -> List[int]:
+    """2232"""
+    def solution1(self, temperatures: list[int]) -> list[int]:
         """ O(n^2) solution"""
         res = [0] * len(temperatures)
 
+        # pylint: disable=consider-using-enumerate
         for i in range(len(temperatures)):
             for j in range(i+1, len(temperatures)):
                 if temperatures[j] > temperatures[i]:
                     res[i] = j - i
                     break
-        
         return res
 
-    def solution2(self, temperatures: List[int]) -> List[int]:
+    def solution2(self, temperatures: list[int]) -> list[int]:
+        """_summary_
+
+        Args:
+            temperatures (list[int]): _description_
+
+        Returns:
+            list[int]: _description_
+        """
         res = [0] * len(temperatures)
-        stack = [] # pair: [temp, index]
+        stack: list[list[int]] = [] # pair: [temp, index]
 
         for i, t in enumerate(temperatures):
             while stack and stack[-1][0] < t:
-                stackT, stackI = stack.pop()
-                res[stackI] = i - stackI
-            
+                stack_t, stack_i = stack.pop()
+                res[stack_i] = i - stack_t
             stack.append([t, i])
-        
         return res
